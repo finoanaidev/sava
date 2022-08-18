613f9bd17f5dfe561b5e12a279b7a0f9353de7aa
Darcia2125 <darciap19.aps2a@gmail.com>
2022-08-18 14:07:12 +0300

    Modification searchPage

--- store/views/searchPage.py
+++ store/views/searchPage.py
@@ -4,6 +4,7 @@ from django.contrib.auth.hashers import  check_password
 from store.models.customer import Customer
 from django.views import  View
 from store.models.product import Products
+from store.models.category import Category
 from django.http import HttpResponse
 
 def SearchPage(request):
@@ -12,6 +13,10 @@ def SearchPage(request):
     params = {'product' : product, 'search' : srh}
     return render(request, 'SearchPage.html', params)
 
-
+def SearchCategory(request):
+    srh = request.GET['query']
+    category = Category.objects.filter(name__icontains=srh)
+    params = {'category' : category, 'search' : srh}
+    return render(request, 'SearchPage.html', params)
 
 
